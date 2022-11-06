53d3616f4ccbb9bb72de4eb29998c8df5ae9cec3
Aryden <106912268+x1yl@users.noreply.github.com>
2022-11-06 21:00:43 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ import got from 'got';
 
 console.time("\nThis took");
 
-//var link = prompt("Enter your link here");
-//var boost = Number(prompt("\nHow many times do you want to boost your profile?"));
-
-var link = "https:camo.githubusercontent.com/fbae0a2cff0eb63113eb6d9d76e5d94855ab37aa5000fd3a1169a08b93bdd531/68747470733a2f2f6b6f6d617265762e636f6d2f67687076632f3f757365726e616d653d7831796c";
-var boost = 20;
+var link = prompt("Enter your link here");
+var boost = Number(prompt("\nHow many times do you want to boost your profile?"));
 
 function error() {
   if (link.includes("http") == false || (Number.isNaN(boost)) || (boost <= 0)) {
